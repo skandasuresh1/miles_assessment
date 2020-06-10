1110aa36521ed91efc8978b113346da21ac02cc0
Skanda Suresh <skandasuresh1@berkeley.edu>
2020-06-10 00:21:11 -0700

    code refactor

--- assessment.js
+++ assessment.js
@@ -16,10 +16,13 @@ const dragover_handler = function(ev) {
 }
 
 const dragenter_handler = function(ev) {
-  if (ev.target.id.substring(0, 2) === row_of_square && !ev.target.id.includes("box")) {
-    ev.target.style.background = "#d4d4d4";
+  const drop_zone = ev.target;
+  if (
+    drop_zone.id.substring(0, 2) === row_of_square
+    && !drop_zone.id.includes("box")
+  ) {
+    drop_zone.style.background = "#d4d4d4";
   }
-
 }
 
 const dragleave_handler = function(ev) {
@@ -28,39 +31,39 @@ const dragleave_handler = function(ev) {
 
 const drop_handler = function (ev) {
   ev.preventDefault();
+  const drop_zone = ev.target;
   const data = ev.dataTransfer.getData("text/plain");
-  const square = document.getElementById(data);
-  const square_reward_num = square.id.substring(0, 2);
-  const col_reward_num = ev.target.id.substring(0, 2);
-  if (square_reward_num === col_reward_num && !ev.target.hasChildNodes()) {
-    ev.target.style.background = "";
-    if (square.id.includes("square")) {
-      const nodeCopy = square.cloneNode(true);
+  const r_square = document.getElementById(data);
+  const reward_num = r_square.id.substring(0, 2);
+  const drop_zone_row = drop_zone.id.substring(0, 2);
+  if (reward_num === drop_zone_row && !drop_zone.hasChildNodes()) {
+    drop_zone.style.background = "";
+    const new_id = drop_zone.id + "box";
+    let dropped_square = r_square;
+    if (r_square.id.includes("original")) {
+      const nodeCopy = r_square.cloneNode(true);
       nodeCopy.addEventListener("dragstart", dragstart_handler);
       nodeCopy.addEventListener("dragend", dragend_handler);
-      nodeCopy.id = ev.target.id + "box";
       nodeCopy.children[0].removeAttribute("hidden");
-      ev.target.appendChild(nodeCopy);
-      nodeCopy.style.opacity = "";
-    } else {
-      square.id = ev.target.id + "box";
-      ev.target.appendChild(square);
-      square.style.opacity = "";
+      dropped_square = nodeCopy;
     }
+    dropped_square.id = new_id;
+    drop_zone.appendChild(dropped_square);
+    dropped_square.style.opacity = "";
   }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   const reward_squares = document.getElementsByClassName("reward_square");
-  const r_drops = document.getElementsByClassName("r_drop");
+  const drop_zones = document.getElementsByClassName("drop_zone");
   Array.from(reward_squares).forEach((square) => {
     square.addEventListener("dragstart", dragstart_handler);
     square.addEventListener("dragend", dragend_handler);
   });
-  Array.from(r_drops).forEach((drop) => {
-    drop.addEventListener("drop", drop_handler);
-    drop.addEventListener("dragover", dragover_handler);
-    drop.addEventListener("dragenter", dragenter_handler);
-    drop.addEventListener("dragleave", dragleave_handler);
+  Array.from(drop_zones).forEach((zone) => {
+    zone.addEventListener("drop", drop_handler);
+    zone.addEventListener("dragover", dragover_handler);
+    zone.addEventListener("dragenter", dragenter_handler);
+    zone.addEventListener("dragleave", dragleave_handler);
   });
 });
